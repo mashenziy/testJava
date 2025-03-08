4a3c2975efb201b51ee1b1abf7f667b7fa8172f9
mariia <mariaamoroz13@gmail.com>
2025-03-08 23:54:22 +0200

    first_lecture

--- untitled/src/main/java/org/example/TestClass.java
+++ untitled/src/main/java/org/example/TestClass.java
@@ -5,5 +5,6 @@ import com.github.javafaker.Faker;
 public class TestClass {
     public static void main(String[] args) {
         System.out.println(Faker.instance().address().fullAddress());
+        System.out.println(Faker.instance().address().fullAddress());
     }
     }
